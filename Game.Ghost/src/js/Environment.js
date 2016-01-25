0724ec6014597b666382ff35d4b8c1f46ae8b00d
Rendxx <rendxx@gmail.com>
2016-01-25 13:00:07 -0600

    Add basic function for three.js

---
+++ Game.Ghost/src/js/Environment.js
@@ -0,0 +1,67 @@
+﻿window.Rendxx = window.Rendxx || {};
+window.Rendxx.Game = window.Rendxx.Game || {};
+window.Rendxx.Game.Ghost = window.Rendxx.Game.Ghost || {};
+
+/**
+ * Setup base environment in three.js 
+ */
+(function (GAME) {
+    /**
+     * Setup environment in three.js
+     * @param {game entity} entity - Game entity
+     * @returns {object} data-pkg of environment 
+     */
+    var setupEnv = function (entity) {
+        if (entity == null) throw new Error('Container not specified.');
+        var scene, camera, renderer;
+        var SCREEN_WIDTH, SCREEN_HEIGHT;
+
+        /*creates empty scene object and renderer*/
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 5000);
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        // add renderer to dom
+        $(entity.domElement).append(renderer.domElement);
+
+        // Render scene on screen
+        var animate = function () {
+            requestAnimationFrame(animate);
+            if (entity.onRender != null) entity.onRender();
+            renderer.render(scene, camera);
+        }
+
+        // set resize
+        var resize = function () {
+            SCREEN_WIDTH = window.innerWidth;
+            SCREEN_HEIGHT = window.innerHeight;
+
+            camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+            camera.updateProjectionMatrix();
+
+            renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        };
+
+        renderer.setClearColor(0x000000);
+        renderer.shadowMapEnabled = true;
+        renderer.shadowMapSoft = true;
+        resize();
+
+        $(window).resize(resize);
+        animate();
+
+        return {
+            scene: scene,
+            camera: camera,
+            renderer: renderer
+        };
+    };
+
+    /**
+     * Setup game with the viewport domelement
+     * @param {game entity} entity - Game entity
+     */
+    GAME.SetupEnv = function (entity) {
+        setupEnv(entity);
+    };
+})(window.Rendxx.Game.Ghost);
---
+++ Game.Ghost/src/js/Setup.js
@@ -0,0 +1,31 @@
+﻿window.Rendxx = window.Rendxx || {};
+window.Rendxx.Game = window.Rendxx.Game || {};
+window.Rendxx.Game.Ghost = window.Rendxx.Game.Ghost || {};
+
+/**
+ * Main entry of Game.Ghost 
+ */
+(function (GAME) {
+    /**
+     * Game Entity
+     */
+    var Entity = function (container, playerNumber) {
+        // data
+        this.domElement = container;
+        this.playerNumber = playerNumber;
+        this.env = null;
+
+        // callback
+        this.onRender = null;
+    };
+
+    /**
+     * Create a game in domElement
+     * @param {dom element} container - Dom element to contain the scene
+     * @param {number} playerNumber - player number
+     */
+    GAME.Create = function (container, playerNumber) {
+        var entity = new Entity(container, playerNumber);
+        entity.env = GAME.SetupEnv(entity);
+    };
+})(window.Rendxx.Game.Ghost);
