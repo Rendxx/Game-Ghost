73275f5a3acf0f1d6622c837b04ddb309d8a2cc4
Rendxx-PC <rendxx@gmail.com>
2016-02-10 01:16:40 -0600

    Export player with animation success

---
+++ Game.Ghost/PlayerControlTest/sample.js
@@ -0,0 +1,164 @@
+﻿$(function () {
+    var scene, camera, renderer;
+    var controls, guiControls, datGUI;
+    var axis, grid, light;
+    var stats;
+    var SCREEN_WIDTH, SCREEN_HEIGHT;
+    var sk_helper;
+
+    var action = {}, mixer, fadeAction;
+
+    var clock = new THREE.Clock();
+    /*variables for lights*/
+
+
+    function init() {
+        /*creates empty scene object and renderer*/
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 5000);
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        renderer.setClearColor(0x000000);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMapEnabled = true;
+        renderer.shadowMapSoft = true;
+
+        /*add controls*/
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', render);
+
+        /*adds helpers*/
+        axis = new THREE.AxisHelper(10);
+        scene.add(axis);
+
+        grid = new THREE.GridHelper(50, 5);
+        grid.setColors(new THREE.Color("rgb(255,0,0)"), 0x000000);
+        scene.add(grid);
+
+        camera.position.x = 40;
+        camera.position.y = 40;
+        camera.position.z = 40;
+        camera.lookAt(scene.position);
+
+        // Ambient
+        light = new THREE.AmbientLight();
+        light.color.setHex(0x999999);
+        scene.add(light);
+
+        // dat gui
+        guiControls = {
+            Def: function () {
+                fadeAction('Def');
+            },
+            Idle1: function () {
+                fadeAction('Idle');
+            },
+            Idel2: function () {
+                fadeAction('Idle2');
+            },
+            Run: function () {
+                fadeAction('Run');
+            },
+            Walk: function () {
+                fadeAction('Walk');
+            }
+        };
+        datGUI = new dat.GUI();
+        datGUI.add(guiControls, 'Def').name('Def');
+        datGUI.add(guiControls, 'Idle1').name('Idle 1');
+        datGUI.add(guiControls, 'Idel2').name('Idel 2');
+        datGUI.add(guiControls, 'Run').name('Run');
+        datGUI.add(guiControls, 'Walk').name('Walk');
+
+        $("#webGL-container").append(renderer.domElement);
+        // status track
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        $("#webGL-container").append(stats.domElement);
+
+        AddBlenderMesh('/Model/player-2.json');
+    }
+
+    function AddBlenderMesh(file) {
+        var loader = new THREE.JSONLoader();
+        var mesh = null;
+        loader.load(file, function (geometry, materials) {
+            for (var i = 0; i < materials.length; i++) {
+                materials[i].skinning = true;
+            }
+            mesh = new THREE.SkinnedMesh(geometry, new THREE.MeshFaceMaterial(materials));
+            mesh.castShadow = true;
+            mesh.receiveShadow = true;
+
+            action.Def = new THREE.AnimationAction(geometry.animations[0]);
+            action.Idle = new THREE.AnimationAction(geometry.animations[1]);
+            action.Idle2 = new THREE.AnimationAction(geometry.animations[2]);
+            action.Run = new THREE.AnimationAction(geometry.animations[3]);
+            action.Walk = new THREE.AnimationAction(geometry.animations[4]);
+            action.Idle.Def = 1;
+            action.Idle.weight = 0;
+            action.Idle2.weight = 0;
+            action.Run.weight = 0;
+            action.Walk.weight = 0;
+
+            mixer = new THREE.AnimationMixer(mesh);
+            mixer.addAction(action.Def);
+            mixer.addAction(action.Idle);
+            mixer.addAction(action.Idle2);
+            mixer.addAction(action.Run);
+            mixer.addAction(action.Walk);
+
+            scene.add(mesh);
+
+            //sk_helper = new THREE.SkeletonHelper(mesh);
+            //scene.add(sk_helper);
+            //mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+            //mesh.castShadow = true;
+            //mesh.receiveShadow = true;
+            //scene.add(mesh);
+        });
+    }
+
+    function render() {
+        /*necessary to make lights function*/
+        //cubeMaterial.needsUpdate = true;
+        //torMaterial.needsUpdate = true;
+    }
+
+
+    fadeAction = function () {
+        var activeActionName = 'Def';
+        return function (name) {
+            if (name == activeActionName) return;
+            mixer.crossFade(action[activeActionName], action[name], .3);
+            activeActionName = name;
+        };
+    }();
+
+
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+        stats.update();
+        if (sk_helper) sk_helper.update();
+
+        var delta = clock.getDelta();
+        if (mixer) mixer.update(delta);
+        renderer.render(scene, camera);
+    }
+
+    $(window).resize(function () {
+        SCREEN_WIDTH = window.innerWidth;
+        SCREEN_HEIGHT = window.innerHeight;
+
+        camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    });
+
+    init();
+    animate();
+});
