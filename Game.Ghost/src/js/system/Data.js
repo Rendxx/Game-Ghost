69798a2910f9944e9ad18207d5281d525f94591e
Rendxx <rendxx@gmail.com>
2016-02-26 15:47:00 -0600

    Start working on game system

--- Game.Ghost/src/js/system/Data.js
+++ Game.Ghost/src/js/system/Data.js
@@ -1,57 +1,128 @@
 ﻿window.Rendxx = window.Rendxx || {};
 window.Rendxx.Game = window.Rendxx.Game || {};
 window.Rendxx.Game.Ghost = window.Rendxx.Game.Ghost || {};
-window.Rendxx.Game.Ghost.Renderer = window.Rendxx.Game.Ghost.Renderer || {};
+window.Rendxx.Game.Ghost.System = window.Rendxx.Game.Ghost.System || {};
 
 /**
  * Data field
  */
-(function (RENDERER) {
+(function (SYSTEM) {
     var Data = {
         character: {
-            survivor: {
-                file: '/Model/player-2.json',
-                action: {
-                    0: 'back',
-                    1: 'init',
-                    2: 'idle',
-                    3: 'idle2',
-                    4: 'run',
-                    5: 'turn',
-                    6: 'walk'
+            parameter: {
+                survivor: {
+                    action: {
+                        0: 'back',
+                        1: 'init',
+                        2: 'idle',
+                        3: 'idle2',
+                        4: 'run',
+                        5: 'turn',
+                        6: 'walk'
+                    },
+                    initAction: 'idle',
+                    light: {
+                        x: 0,
+                        y: 7,
+                        z: 1,
+                        intensity: 1,
+                        distance: 20,
+                        angle: 1,
+                        exponent: 10,
+                        color: 0xffffff
+                    },
+                    torch: {
+                        x: -1.4,
+                        y: 2.4,
+                        z: 0.8,
+                        color: 0xffffff,
+                        intensity: 1,
+                        distance: 50,
+                        angle: 1,
+                        exponent: 8,
+                        shadowCameraNear: 1,
+                        shadowCameraFar: 50,
+                        shadowCameraFov: 50,
+                        shadowCameraVisible: false,
+                        shadowMapWidth: 2056,
+                        shadowMapHeight: 2056,
+                        shadowBias: 0.00,
+                        shadowDarkness: 1.0
+                    }
                 },
-                initAction: 'idle',
-                light: {
-                    x: 0,
-                    y: 7,
-                    z: 1,
-                    intensity: 1,
-                    distance: 20,
-                    angle: 1,
-                    exponent: 10,
-                    color: 0xffffff
-                },
-                torch:{
-                    x : -1.4,
-                    y : 2.4,
-                    z : 0.8,
-                    color: 0xffffff,
-                    intensity : 1,
-                    distance : 50,
-                    angle : 1,
-                    exponent : 8,
-                    shadowCameraNear : 1,
-                    shadowCameraFar : 50,
-                    shadowCameraFov : 50,
-                    shadowCameraVisible : false,
-                    shadowMapWidth : 2056,
-                    shadowMapHeight : 2056,
-                    shadowBias : 0.00,
-                    shadowDarkness : 1.0
+                ghost: {
+
                 }
+            },
+            files: {
+                survivor: {
+                    'green': '/green/player-2.json',
+                },
+                ghost: {
+
+                }
+            },
+            path: '/Model/Player/'
+        },
+        item: {
+            categoryName: {
+                wall: 'wall',
+                ground: 'ground',
+                furniture: 'furniture',
+                stuff: 'stuff',
+                door: 'door'
+            },
+            categoryOrder: [
+                'stuff',
+                'furniture',
+                'door',
+                'wall',
+                'ground'
+            ],
+            files: {
+                wall: {
+                    'White Wall': 'wall_white.data.json'
+                },
+                ground: {
+                    'Wood': 'ground_wood.data.json',
+                    'Ceramic': 'ground_ceramic.data.json'
+                },
+                furniture: {
+                    'Table (Wood) 1': 'table_wood_1_1.data.json',
+                    'Table (Wood) 2': 'table_wood_2_1.data.json',
+                    'Table (Wood) 3': 'table_wood_3_1.data.json',
+                    'Table (Wood) 4': 'table_wood_3_2.data.json',
+                    'Shelf (Wood)': 'shelf_wood_2_1.data.json',
+                    'Cabinet (Wood) 1': 'cabinet_wood_1_1.data.json',
+                    'Cabinet (Wood) 2': 'cabinet_wood_2_1.data.json',
+                    'Cabinet (Wood) 3': 'cabinet_wood_2_1_b.data.json',
+                    'Chair (Wood) 1': 'chair_wood_1_1.data.json',
+                },
+                stuff: {
+                    'Vase 1': 'vase_1.data.json',
+                    'Box 1': 'box_1.data.json',
+                    'Books 1': 'books_1.data.json',
+                    'Key 1': 'key_1.data.json',
+                },
+                door: {
+                    'Door (Wood) 1': 'door_wood_1.data.json',
+                }
+            },
+            path: {
+                wall: '/Model/Environment/',
+                ground: '/Model/Environment/',
+                furniture: '/Model/Furniture/',
+                stuff: '/Model/Stuff/',
+                door: '/Model/Door/'
             }
+        },
+        map: {
+            files:{
+
+            },
+            path: '/GameData/Map/'
         }
     };
 
-    RENDERER.Data = Data;
-})(window.Rendxx.Game.Ghost.Renderer);
+    SYSTEM.Data = Data;
+})(window.Rendxx.Game.Ghost.System);
---
+++ Game.Ghost/src/js/system/FileLoader.js
@@ -0,0 +1,121 @@
+﻿window.Rendxx = window.Rendxx || {};
+window.Rendxx.Game = window.Rendxx.Game || {};
+window.Rendxx.Game.Ghost = window.Rendxx.Game.Ghost || {};
+window.Rendxx.Game.Ghost.System = window.Rendxx.Game.Ghost.System || {};
+
+/**
+ * Load data file:
+ *  Characters
+ *  Items
+ *  Map
+ */
+(function (SYSTEM) {
+    var Data = SYSTEM.Data;
+    var FileLoader = function () {
+        // data ----------------------------------------------------------
+        var that = this,
+            loadCount = 0,
+            loadedCount = 0,
+            _data_basic = null;
+
+        // callback ------------------------------------------------------
+        this.onLoaded = null;
+
+        // public method -------------------------------------------------
+        // load basic files
+        this.loadBasic = function () {
+            if (_data_basic == null) _data_basic = {};
+            loadCount = 1;
+
+            // items
+            var items = Data.item.files;
+            _data_basic.items = {};
+            for (var category in items) {
+                _data_basic.items[category] = {};
+                for (var name in items[category]) {
+                    loadCount++;
+                    $.getJSON(Data.item.path[category] + items[category][name], function (data) {
+                        if (data == null) throw new Error(category + '.' + name + ': Not found.');
+                        if (_data_basic.items[category][name] != null) console.log(category + '.' + name + ': load multiple data.');
+                        _data_basic.items[category][name] = data;
+                        loadedCount++;
+                        _onloaded();
+                    });
+                }
+            }
+
+            // character
+            var characters = Data.character.files;
+            _data_basic.characters = {};
+            for (var role in characters) {
+                _data_basic.characters[role] = {};
+                for (var name in characters[role]) {
+                    loadCount++;
+                    $.getJSON(Data.character.path[role] + characters[role][name], function (data) {
+                        if (data == null) throw new Error(role + '.' + name + ': Not found.');
+                        if (_data_basic.characters[role][name] != null) console.log(role + '.' + name + ': load multiple data.');
+                        _data_basic.characters[role][name] = data;
+                        loadedCount++;
+                        _onloaded();
+                    });
+                }
+            }
+            
+            // on loaded
+            var _onloaded = function () {
+                if (loadedCount >= loadCount) {
+                    that.onLoaded(_data_basic);
+                }
+            };
+            loadCount--;
+            _onloaded();
+        };
+
+        // load map data
+        this.loadMap = function (file, onSuccess, onError) {
+            $.getJSON(Data.map.path + file, function (data) {
+                try{
+                    if (data == null) throw new Error(category + '.' + name + ': Not found.');
+                    if (onSuccess != null) onSuccess(data);
+                } catch (e) {
+                    if (onError!=null) onError(e);
+                }
+            });
+        };
+
+        // private method ------------------------------------------------
+        var _loadItemData = function (category, name, file) {
+            loadCount++;
+            $.getJSON(Data.path[category] + file, function (data) {
+                if (data == null) throw new Error(category + '.' + name + ': Not find.');
+                if (itemData[category][name] != null) console.log(category + '.' + name + ': load multiple data.');
+                itemData[category][name] = data;
+                var ele = $(Data.html.itemSelector).html((data.dimension != null ? '[' + data.dimension[0] + '*' + data.dimension[1] + ']&nbsp;&nbsp;' : '') + '<b>' + name + '</b>').appendTo(_html.selectorCategory[category])
+                    .click(function (e) {
+                        if (lastSelect != null) lastSelect.removeClass('hover');
+                        lastSelect = ele;
+                        ele.addClass('hover');
+                        if (that.onChange) that.onChange(data);
+                    });
+                itemData[category][name].ele = ele;
+                itemData[category][name].category = category;
+                loadedCount++;
+                if (loadedCount >= loadCount) {
+                    _onLoaded();
+                }
+            });
+        };
+
+        var _init = function () {
+        };
+        _init();
+    };
+
+    /**
+     * Game map
+     * @param {number} id - player id
+     * @param {string} name - player name
+     * @param {object} para - character parameters
+     */
+    SYSTEM.FileLoader = FileLoader
+})(window.Rendxx.Game.Ghost.System);
