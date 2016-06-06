1148fe606bf2962bc23b0788e58acc4f8e5caa38
Rendxx-PC <rendxx@gmail.com>
2016-06-05 23:06:07 -0500

    Fix bobo blender file and new png seq complete

--- Game.Ghost/test/game.js
+++ Game.Ghost/test/game.js
@@ -3,7 +3,7 @@
 $(function () {
     // game -----------------------------------------------------
     var _root = null;
-    var renderer = window.Rendxx.Game.Ghost.Renderer.Create(document.getElementById('game-container'), _root, ['p7'], { 0: true, 2: false });
+    var renderer = window.Rendxx.Game.Ghost.Renderer.Create(document.getElementById('game-container'), _root, ['p7'], { 0: false, 1: true });
     var system = window.Rendxx.Game.Ghost.System.Create(_root, "../js/Game.Ghost.System.Core.js");
     system.onSetuped = function (setupData) {
         renderer.reset(setupData);
@@ -106,5 +106,5 @@ $(function () {
     // helper --------------------------------------------------------
     HELPER.addStats($('#game-container'));
     HELPER.addDatGUI($('body'));
-    SetupControl(system, 'p7');
+    SetupControl(system, 'p1');
 });
